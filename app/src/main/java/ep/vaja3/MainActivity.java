e160a0be23d9fc92a8b28d0375760c84c2d86f8b
David Jelenc <david.jelenc@fri.uni-lj.si>
2018-11-26 10:18:00 +0100

    Updates android version

--- app/src/main/java/ep/vaja3/MainActivity.java
+++ app/src/main/java/ep/vaja3/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = (TextView) findViewById(R.id.textView);
+        textView = findViewById(R.id.textView);
     }
 
     public void increment(View view) {
